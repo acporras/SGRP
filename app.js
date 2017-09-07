475bb1946edbe27119baf3c50af0df7b802f738c
Alex JC <acaicedoporras@gmail.com>
2017-09-07 18:50:02 -0500

    change npm script 3

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	hbs = require('express-handlebars'),
+	cors = require('cors'),
+	methodOverride = require('method-override');
+
+const api = require('./routes');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('.hbs', hbs({
+  defaultLayout: 'default',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.use(methodOverride());
+app.use(cors());
+
+app.use('/api',api);
+
+module.exports = app
