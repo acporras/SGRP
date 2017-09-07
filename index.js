6b8a611afbe33c3ea1cb333782ef3bc011d2b977
Alex JC <acaicedoporras@gmail.com>
2017-09-07 18:57:56 -0500

    change npm script 4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require('./config')
 
 mongoose.connect(config.db, (err, res) => {
   if (err) {
-    return console.log('Error al conectar a la base de datos: ${err}')
+    return console.log(`Error al conectar a la base de datos: ${err}`)
   }
   console.log('Conexión a la base de datos establecida...')
 
