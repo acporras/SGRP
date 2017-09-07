c9d8b888c016577324e1cfd434ede87cd2e467d9
Alex JC <acaicedoporras@gmail.com>
2017-09-07 18:02:45 -0500

    preparing to Heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-	port: process.env.PORT || 3000
-	db: process.env.MONGODB || 'mongodb://localhost/sgrpprod_desa'
+	port: process.env.PORT || 3000,
+	db: process.env.MONGODB || 'mongodb://localhost/sgrpprod_desa',
  	TOKEN_SECRET: process.env.TOKEN_SECRET || "tokenaccess"
 };
