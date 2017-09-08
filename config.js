f79155988a4df1ff6c1c9de1fc79c8ec1b36cc27
Alex JC <acaicedoporras@gmail.com>
2017-09-07 19:09:58 -0500

    change npm script 5

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost/sgrpprod_desa',
+	db: process.env.MONGODB_URI || 'mongodb://localhost/sgrpprod_desa',
  	TOKEN_SECRET: process.env.TOKEN_SECRET || "tokenaccess"
 };
